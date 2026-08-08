d5e93248dc44bdcb4cc69c754caca709fc0ab31b
rajureddy <rajureddyhadole452@gmail.com>
2026-08-08 10:39:59 +0530

    added a test

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
-
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from tasks.models import Task
+from django.contrib.auth import get_user_model
 # Create your tests here.
+
+User = get_user_model()
+
+class TaskAPITestCase(APITestCase):
+  def setUp(self):
+    self.authenticated_user = User.objects.create_user(
+      username="user",
+      password="pass123"
+    )
+
+    self.normal_user = User.objects.create_user(
+      username="other",
+      password="pass123"
+    )
+
+    self.url = reverse("task-list")
+
+  def test_authenticated_user_can_access_tasks(self):
+
+    self.client.force_authenticate(user=self.authenticated_user)
+
+    response = self.client.get(self.url)
+
+    self.assertEqual(response.status_code, 200)
+
+
+  def test_unauthenticated_user_cannot_access_tasks(self):
+
+    response = self.client.get(self.url)
+
+    self.assertEqual(response.status_code, 401)
+
+  
