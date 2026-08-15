304cbaebfc6b4a54b9a8cc0da4548877a634741b
rajureddy <rajureddyhadole452@gmail.com>
2026-08-15 13:27:34 +0530

    added small changes

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': 'db',
-        'PORT': '5432',
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
